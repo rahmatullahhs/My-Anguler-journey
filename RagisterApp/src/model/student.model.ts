d76449ff5d0b19b2a75dc009c823113b88c5ea0e
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-06-24 19:14:10 +0600

    add

---
+++ RagisterApp/src/model/student.model.ts
@@ -0,0 +1,9 @@
+export class Employee{
+    private id!: number;
+    private name!: string;
+    private email!: string;
+    private phone!: string;
+    private gender!: string;
+    private designation!: string;
+    private salary!: number;
+}
