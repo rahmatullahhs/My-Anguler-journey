885aa432f7859d7ecd7b251f6f6b6da13d7f7e5b
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-06-28 12:35:02 +0600

    add

--- myapp/src/app/service/student.service.ts
+++ myapp/src/app/service/student.service.ts
@@ -8,12 +8,25 @@ import { Student } from '../model/student.model';
   providedIn: 'root'
 })
 export class StudentService {
-
+// is simply declaring a class property (often in a service) that stores the API endpoint's base URL.
+//  We use it so that when you make HTTP requests (e.g., GET, POST, etc.), 
+// you don’t have to hardcode the full URL every time.
+// endpoint is a specific address where you make requests.
   baseUrl: string = "http://localhost:3000/students";
-
+// you’re using Angular’s dependency injection to get an instance of the HttpClient service into your class 
+// (whether it's a component or a service).
   constructor(private http: HttpClient) { }
 
+// GET → read data
+// POST → create data
+// PUT / PATCH → update data
+// DELETE → delete data
 
+//  Fetches data from your API
+// It calls your API at the baseUrl, using HttpClient.
+// It returns an Observable that:
+// Defers execution until subscribed.
+// Emits exactly one response (a list of students) then completes.
   getAllStudent(): Observable<any> {
     return this.http.get(this.baseUrl);
   }
