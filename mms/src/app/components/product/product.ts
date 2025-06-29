42731cd922e357fb7c4a790fae565681ae2f028b
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-06-29 18:58:09 +0600

    add

--- mms/src/app/components/product/product.ts
+++ mms/src/app/components/product/product.ts
@@ -9,3 +9,56 @@ import { Component } from '@angular/core';
 export class Product {
 
 }
+    // Desktop
+// Desktop Offer
+// Star PC
+// Gaming PC
+// Brand PC
+// All-in-One PC
+// Portable Mini PC
+// Apple Mac Mini
+// Apple iMac
+// Apple Mac Studio
+// Apple Mac Pro
+// Show All Desktop
+
+// Laptop
+// All Laptop
+// Gaming Laptop
+// Premium Ultrabook
+// Laptop Bag
+// Laptop Accessories
+// Show All Laptop
+
+// Laptop
+//       All Laptop
+// MSI
+// Lenovo
+// Asus
+// HP
+// Acer
+// Smart
+// Dell
+// MacBook
+// Microsoft
+// Infinix
+// Chuwi
+// Gigabyte
+// Walton
+// AOC
+
+
+
+// All Laptop
+// Gaming Laptop
+// Premium Ultrabook
+// Laptop Bag
+//         Laptop Accessories
+// Laptop Cooler
+// Laptop Desk
+// Laptop Stand
+// Laptop Battery
+// Laptop Charger / Adapter
+// Display
+// Laptop Keyboard
+// Caddy
