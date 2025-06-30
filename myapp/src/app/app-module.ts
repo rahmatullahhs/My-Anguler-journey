8d76fa0319ac43053a8001501e2527c4cfb1e05d
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-06-30 18:23:55 +0600

    add

--- myapp/src/app/app-module.ts
+++ myapp/src/app/app-module.ts
@@ -4,23 +4,29 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Home } from './home/home';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { Product } from './components/product.component/product/product';
+import { UpdateStudent } from './components/student.component/update-student/update-student';
+import { ViewAllStudent } from './components/student.component/view-all-student/view-all-student';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
-import { Product } from './components/product.component/product/product';
 
 @NgModule({
   declarations: [
     App,
     Home,
-    Product
+    Product,
+    UpdateStudent,
+    ViewAllStudent,
+
+  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule
-  ],
+],
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
--- myapp/src/app/components/student.component/update-student/update-student.ts
+++ myapp/src/app/components/student.component/update-student/update-student.ts
@@ -9,53 +9,53 @@ import { Student } from '../../../model/student.model';
   templateUrl: './update-student.html',
   styleUrl: './update-student.css'
 })
-export class UpdateStudent implements OnInit{
+export class UpdateStudent implements OnInit {
 
-id:string='';
-student: Student = new Student();
-// studentService: A service that interacts with the backend API for student data.
-// router: Used to navigate between pages.
-// route: Used to access route parameters (like the student ID).
-// cdr (ChangeDetectorRef): Manually triggers change detection in case Angular doesn't detect changes automatically.
+  id: string = '';
+  student: Student = new Student();
+  // studentService: A service that interacts with the backend API for student data.
+  // router: Used to navigate between pages.
+  // route: Used to access route parameters (like the student ID).
+  // cdr (ChangeDetectorRef): Manually triggers change detection in case Angular doesn't detect changes automatically.
 
-constructor(
-private studentService: StudentService,
-private router:Router,
-private route:ActivatedRoute,
-private cdr : ChangeDetectorRef
+  constructor(
+    private studentService: StudentService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private cdr: ChangeDetectorRef
 
-){}
+  ) { }
 
   ngOnInit(): void {
-  this.loadStudentByid();
+    this.loadStudentByid();
   }
 
-loadStudentByid(){
+  loadStudentByid() {
 
-  this.id = this.route.snapshot.params['id'];
+    this.id = this.route.snapshot.params['id'];
 
-  this.studentService.getStudentById(this.id).subscribe({
-next:(res)=>{
-this.student=res;
-this.cdr.markForCheck();
-},
-error:(err)=>{
-console.error('error fetching student: ',err);
-}
+    this.studentService.getStudentById(this.id).subscribe({
+      next: (res) => {
+        this.student = res;
+        this.cdr.markForCheck();
+      },
+      error: (err) => {
+        console.error('error fetching student: ', err);
+      }
 
-  });
-  
-}
+    });
 
-updateStudent():void{
-this.studentService.updateStudent(this.id,this.student).subscribe({
+  }
 
-next:(res)=>this.router.navigate(['/allstu']),
-error:(err)=>console.error('update failed',err)
-});
+  updateStudent(): void {
+    this.studentService.updateStudent(this.id, this.student).subscribe({
+
+      next: (res) => this.router.navigate(['/allstu']),
+      error: (err) => console.error('update failed', err)
+    });
 
 
-}
+  }
 
 
 
