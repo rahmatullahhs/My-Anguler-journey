29ffe04e61026c7115c7217afdfb1c686ac1c515
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-07-05 16:40:03 +0600

    add

--- Smart-mms/src/app/app-module.ts
+++ Smart-mms/src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Products } from './product/products/products';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Products
   ],
   imports: [
     BrowserModule,
