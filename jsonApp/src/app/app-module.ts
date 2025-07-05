a75adf0d3eb0186c23aa613a9b7347bffb31ab8c
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-07-05 15:18:40 +0600

    add

--- jsonApp/src/app/app-module.ts
+++ jsonApp/src/app/app-module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Analytics } from './components/dashboard/analytics/analytics';
+import { Home } from './components/home/home';
 
 @NgModule({
   declarations: [
     App,
-    Analytics
+    Analytics,
+    Home
   ],
   imports: [
     BrowserModule,
