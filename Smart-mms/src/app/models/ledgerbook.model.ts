33060c9d526f220c4f2aaed5cbaeb4188b9d8004
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-07-15 18:59:21 +0600

    add

--- Smart-mms/src/app/models/ledgerbook.model.ts
+++ Smart-mms/src/app/models/ledgerbook.model.ts
@@ -1,2 +1,12 @@
 export class LedgerbookModel {
+    
+  id!: number;                 
+  date!: string;             
+  description!: string;         
+  accountType!: 'Asset' | 'Liability' | 'Equity' | 'Revenue' | 'Expense';
+  debit!: number;               
+  credit!: number;             
+  balance!: number;           
+
+
 }
