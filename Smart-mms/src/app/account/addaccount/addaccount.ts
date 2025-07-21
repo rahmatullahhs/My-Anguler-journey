2a04a2cb4e22bd4644e33098788badf3df5c80ca
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-07-21 11:12:50 +0600

    add

--- Smart-mms/src/app/account/addaccount/addaccount.ts
+++ Smart-mms/src/app/account/addaccount/addaccount.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-addaccount',
@@ -9,29 +9,4 @@ import { Router } from '@angular/router';
 })
 export class Addaccount {
 
-  constructor(private router: Router) {}
-
-  goTo(page: string) {
-    switch (page) {
-      case 'master-ledger':
-        this.router.navigate(['/master-ledger']);
-        break;
-      case 'billstracking':
-        this.router.navigate(['/bill-records']);
-        break;
-      case 'sales-records':
-        this.router.navigate(['/sales-records']);
-        break;
-      case 'cogs':
-        this.router.navigate(['/cogs']);
-        break;
-      case 'product-price':
-        this.router.navigate(['/product-price']);
-        break;
-      default:
-        console.warn('Unknown page:', page);
-    }
-  }
 }
-
-
