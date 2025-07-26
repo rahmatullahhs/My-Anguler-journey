cfb2648409e3927972a14e55006941c051b157c2
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-07-26 23:18:47 +0600

    add

--- management-system/src/app/app-module.ts
+++ management-system/src/app/app-module.ts
@@ -1,16 +1,29 @@
 import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+
+
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Dashboard } from './appface/dashboard/dashboard';
+import { Sidebar } from './appface/sidebar/sidebar';
+import { Navbar } from './appface/navbar/navbar';
+import { NgChartsModule } from 'ng2-charts';
 
+
+
+const chartsModule = NgChartsModule;
 @NgModule({
   declarations: [
-    App
+    App,
+    Dashboard,
+    Sidebar,
+    Navbar
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+   NgChartsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
--- management-system/src/app/app-routing-module.ts
+++ management-system/src/app/app-routing-module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Dashboard } from './appface/dashboard/dashboard';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: '', component: Dashboard }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
