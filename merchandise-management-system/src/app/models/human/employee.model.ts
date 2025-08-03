fea65285c42954be076ac0a77db6346c294f7145
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-08-03 18:03:40 +0600

    add

--- merchandise-management-system/src/app/models/human/employee.model.ts
+++ merchandise-management-system/src/app/models/human/employee.model.ts
@@ -1,2 +1,14 @@
-export class EmployeeModel {
+export interface EmployeeModel {
+
+    id: string;
+    name: string;
+    email: string;
+    phone: string;
+    nid:string;
+    gender: string;
+    designation: string;
+    address: string;
+    photoUrl?: string;
+    salary: number;
+
 }
--- merchandise-management-system/src/app/service/mankind/employee.service.ts
+++ merchandise-management-system/src/app/service/mankind/employee.service.ts
@@ -1,8 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { EmployeeModel } from '../../models/human/employee.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
   
+baseUrl:string="http://localhost:3001/employee"
+constructor(private http: HttpClient){}
+
+getAllEmp():Observable<any>{
+  return this.http.get(this.baseUrl);
+}
+addEmp(employee:EmployeeModel):Observable<any>{
+return this.http.post(this.baseUrl,employee);
+}
+updateEmp(id:string,employee:EmployeeModel):Observable<any>{
+return this.http.put(this.baseUrl+"/"+id,employee);
+}
+deleteEmp(id:string):Observable<any>{
+  return this.http.delete(this.baseUrl+"/"+id);
+}
+
+
+
+
+
+
+
 }
