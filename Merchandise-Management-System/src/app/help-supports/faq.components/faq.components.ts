61c856b98367a81dd893031c5a077a9f15f8229e
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-08-24 19:04:38 +0600

    add

---
+++ Merchandise-Management-System/src/app/help-supports/faq.components/faq.components.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faq.components',
+  standalone: false,
+  templateUrl: './faq.components.html',
+  styleUrl: './faq.components.css'
+})
+export class FaqComponents {
+
+}
