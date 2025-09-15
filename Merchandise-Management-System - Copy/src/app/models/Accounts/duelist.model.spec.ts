97fb8dcd25fd82e42425fc60e0a545bc6929d860
Rahmat Ullah <mdrahmatullahshahrukh@gmail.com>
2025-09-15 10:26:44 +0600

    d

--- Merchandise-Management-System - Copy/src/app/models/Accounts/duelist.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DuelistModel } from './duelist.model';
-
-describe('DuelistModel', () => {
-  it('should create an instance', () => {
-    expect(new DuelistModel()).toBeTruthy();
-  });
-});
--- Merchandise-Management-System - Copy/src/app/models/help-support/faq.model.ts
+++
@@ -1,7 +0,0 @@
-export interface FaqModel {
-      question: string;
-  answer: string;
-  isOpen?: boolean;
-}
-
-
